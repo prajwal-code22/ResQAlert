3a1e67eb8d4e3658f4eecc4b2e1b1cfd1f2cc2ce
Prajwal <prajwalsiwakoti39@gmail.com>
2026-02-25 10:17:31 +0545

    Implement custom user admin interface with additional fields and filters

--- ResqAlert/accounts/admin.py
+++ ResqAlert/accounts/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+
+    model = User
+
+    # Fields shown when editing existing user
+    fieldsets = UserAdmin.fieldsets + (
+        ('Extra Information', {
+            'fields': ('role', 'phone', 'responder_category', 'is_available'),
+        }),
+    )
+
+    # Fields shown when creating new user
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Extra Information', {
+            'fields': ('role', 'phone', 'responder_category', 'is_available'),
+        }),
+    )
+
+    list_display = ('username', 'email', 'role', 'phone', 'is_available')
+    list_filter = ('role', 'is_available')
+
+
+admin.site.register(User, CustomUserAdmin)
