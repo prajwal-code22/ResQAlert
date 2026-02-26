f8c52b11c08e2632430f7e76f4dd84a9c5d0274b
Prajwal <prajwalsiwakoti39@gmail.com>
2026-02-26 23:09:00 +0545

    Refactor models to include UserLocation, UserProfile, EmergencyRequest, EmergencyResponder, and DeviceToken with appropriate string representations

--- ResqAlert/accounts/models.py
+++ ResqAlert/accounts/models.py
@@ -27,3 +27,42 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+    
+class UserLocation(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='location')
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return f"{self.user.username}'s Location"
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+
+class EmergencyRequest(models.Model):
+    victim = models.ForeignKey(User, on_delete=models.CASCADE, related_name='emergency_requests')
+    responder = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='assigned_requests')
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    category = models.CharField(max_length=20)
+    status = models.CharField(max_length=20, default='pending')
+    timestamp = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"Emergency by {self.victim.username}"
+
+class EmergencyResponder(models.Model):
+    request=models.ForeignKey(EmergencyRequest, on_delete=models.CASCADE, related_name='responders')
+    responder=models.ForeignKey(User, on_delete=models.CASCADE, related_name='emergency_responses')
+    status=models.CharField(max_length=20, defalut='notified')
+    notified_at=models.DateTimeField(auto_now_add=True)
+    
+class DeviceToken(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='device_token')
+    token = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.user.username}'s Device Token"
