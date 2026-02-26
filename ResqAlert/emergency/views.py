dc52ae7aafd0927f2da663d665c277b660c90e9c
Prajwal <prajwalsiwakoti39@gmail.com>
2026-02-26 23:09:28 +0545

    Implement emergency request handling with validation for latitude, longitude, and category

--- ResqAlert/emergency/views.py
+++ ResqAlert/emergency/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.utils import timezone
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from datetime import timedelta
+from accounts.models import EmergencyRequest, EmergencyResponder
+from find_responder import find_nearby_responders
+from .tasks import process_emergency_request
 
-# Create your views here.
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def emergency_request(request):
+
+    user = request.user
+
+    latitude = request.data.get('latitude')
+    longitude = request.data.get('longitude')
+    category = request.data.get('category')
+
+    if not latitude or not longitude or not category:
+        return Response(
+            {"message": "Latitude, longitude and category are required"},
+            status=400
+        )
+
+    
+    emergency = EmergencyRequest.objects.create(
+        victim=user,
+        latitude=latitude,
+        longitude=longitude,
+        category=category,
+        status="searching"
+    )
+
+    process_emergency_request.delay(emergency.id)
+
+
