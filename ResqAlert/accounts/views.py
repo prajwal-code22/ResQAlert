d3df586d1c56e1375d46332d8195761f26af95df
Prajwal <prajwalsiwakoti39@gmail.com>
2026-03-02 00:14:05 +0545

    Add save_device view for managing user device information

--- ResqAlert/accounts/views.py
+++ ResqAlert/accounts/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework.response import Response
+from .models import UserDevice
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def save_device(request):
+
+    token = request.data.get("fcm_token")
+    device_type = request.data.get("device_type")
+
+    if not token:
+        return Response({"message": "Token required"}, status=400)
+
+    # Create or update device
+    UserDevice.objects.update_or_create(
+        fcm_token=token,
+        defaults={
+            "user": request.user,
+            "device_type": device_type,
+            "is_active": True
+        }
+    )
+
+    return Response({"message": "Device saved"}) 
