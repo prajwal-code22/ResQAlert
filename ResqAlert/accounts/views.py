79a963800ab0ef56b88605987e4924b354c6173e
Prajwal <prajwalsiwakoti39@gmail.com>
2026-03-03 22:27:34 +0545

    funxtion create for registration

--- ResqAlert/accounts/views.py
+++ ResqAlert/accounts/views.py
@@ -3,6 +3,23 @@ from rest_framework.decorators import api_view,permission_classes
 from rest_framework.response import Response
 from .models import UserDevice
 from rest_framework.permissions import IsAuthenticated
+from .models import User
+from .serializers import UserSerializer
+
+
+
+#resgister the user
+@api_view(['POST'])
+def register(request):
+    serializer= UserSerializer(data=request.data)
+    if serializer.is_valid():
+        user=serializer.save()
+        return Response({"message":"User registered successfully"}, status=201)
+    return Response(serializer.errors, status=400)
+    
+    
+
+
 
 # Create your views here.
 @api_view(['POST'])
