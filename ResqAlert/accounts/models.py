682e263e801eacbcfedd53e5a83ce30e91a33f04
Prajwal <prajwalsiwakoti39@gmail.com>
2026-03-03 22:32:12 +0545

    fixed models

--- ResqAlert/accounts/models.py
+++ ResqAlert/accounts/models.py
@@ -59,22 +59,6 @@ class UserProfile(models.Model):
     def __str__(self):
         return f"{self.user.username}'s Profile"
 
-class EmergencyRequest(models.Model):
-    victim = models.ForeignKey(User, on_delete=models.CASCADE, related_name='emergency_requests')
-    responder = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='assigned_requests')
-    latitude = models.FloatField()
-    longitude = models.FloatField()
-    category = models.CharField(max_length=20)
-    status = models.CharField(max_length=20, default='pending')
-    timestamp = models.DateTimeField(auto_now_add=True)
-    def __str__(self):
-        return f"Emergency by {self.victim.username}"
-
-class EmergencyResponder(models.Model):
-    request=models.ForeignKey(EmergencyRequest, on_delete=models.CASCADE, related_name='responders')
-    responder=models.ForeignKey(User, on_delete=models.CASCADE, related_name='emergency_responses')
-    status=models.CharField(max_length=20, defalut='notified')
-    notified_at=models.DateTimeField(auto_now_add=True)
     
 class DeviceToken(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='device_token')
