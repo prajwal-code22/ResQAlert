9f5883f2f096d80164672a152f12914630e9c17b
Prajwal <prajwalsiwakoti39@gmail.com>
2026-03-03 22:27:13 +0545

    url create for register

--- ResqAlert/accounts/urls.py
+++ ResqAlert/accounts/urls.py
@@ -1,7 +1,8 @@
 # to save user fcm_token and device type for push notifications
 from django.urls import path
-from .views import save_device
+from .views import save_device, register
 
 urlpatterns = [
+    path('register/', register, name='register'),
     path("save-device/", save_device, name="save_device"),
 ]
